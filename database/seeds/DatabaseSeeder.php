5c013654f79c15d8697659a0400d87ceb9928abb
Oscar Llanes <oellanes88@gmail.com>
2017-10-05 12:54:13 -0300

    Agrega tabla de provincias.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call('RolesTableSeeder');
         $this->call('RegistrosTableSeeder');
         $this->call('UsuariosTableSeeder');
+        $this->call('ProvinciasTableSeeder');
 
         Schema::enableForeignKeyConstraints();
     }
--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -33,13 +33,13 @@
         <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
         <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
         <li class="treeview">
-            <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+            <a href="#"><i class="fa fa-link"></i> <span>Informes</span>
                 <span class="pull-right-container">
                     <i class="fa fa-angle-left pull-right"></i>
                 </span>
             </a>
             <ul class="treeview-menu">
-                <li><a href="#">Link in level 2</a></li>
+                <li><a href="#">Nuevo</a></li>
                 <li><a href="#">Link in level 2</a></li>
             </ul>
         </li>
